25780bf3c944d7d3aef6f343caa5bfd0927ee72d
Yoshiyuki Canoy <ycanoy@PHILKOEI.COM.PH>
2025-06-25 17:14:43 +0800

    nice

--- src/components/DyanamicPage/bodyDiv.jsx
+++ src/components/DyanamicPage/bodyDiv.jsx
@@ -1,20 +1,34 @@
 export default function BodyDiv() {
        const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    return <>
-        
-            <form className="BodyDiv">
-                <input type="text" className="BodyDivInput" placeholder="e.g. paprika" />
-                <button className="BodyDivButton">+ Add Ingredient</button>
+       const listedingredients = ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+       ))
+   
+       function handleSubmit(event){
+        event.preventDefault();
+        console.log("iwas clicked");
+      }
+
+
+    return (
+    <main>
+             <form onSubmit={handleSubmit} className="BodyDiv"  >
+                <input type="text" 
+                className="BodyDivInput" 
+                placeholder="e.g. paprika"
+                name = "ingredient"
+                />
+
+                <button type = "submit" className="BodyDivButton">
+                    + Add Ingredient
+                </button>
             </form>
 
             <ul>
-    {ingredients.map((ingredient, index)=>(
-                <li key={index} className="BodyDivListItem">
-                    <span className="BodyDivListItemText">{ingredient}</span>
-                    <button className="BodyDivListItemButton">X</button>
-                </li>
-    ))}
+                {listedingredients}
             </ul>
     
-    </>
+    </main>
+    );
 }
+
